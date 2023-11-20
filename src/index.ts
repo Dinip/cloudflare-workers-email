cd041bd6cfa8788c513f51171dd833036262d36d
Dinip <8941012+Dinip@users.noreply.github.com>
2023-11-20 19:46:32 +0000

    added auth token and more fields

--- src/index.ts
+++ src/index.ts
@@ -1,71 +1,80 @@
 export interface Env {
-	// Example binding to KV. Learn more at https://developers.cloudflare.com/workers/runtime-apis/kv/
-	// MY_KV_NAMESPACE: KVNamespace;
-	//
-	// Example binding to Durable Object. Learn more at https://developers.cloudflare.com/workers/runtime-apis/durable-objects/
-	// MY_DURABLE_OBJECT: DurableObjectNamespace;
-	//
-	// Example binding to R2. Learn more at https://developers.cloudflare.com/workers/runtime-apis/r2/
-	// MY_BUCKET: R2Bucket;
 	DKIM_DOMAIN: string
 	DKIM_SELECTOR: string
 	DKIM_PRIVATE_KEY: string
 	EMAIL_DEFAULT_FROM: string
 	EMAIL_DEFAULT_FROMNAME: string
+	AUTH_TOKEN: string
 }
 
-
 type email = {
-	to: string
+	to: sendTo[]
+	cc?: sendTo[]
+	bcc?: sendTo[]
 	from?: string
 	fromName?: string
+	replyTo?: string
 	subject: string
 	html: boolean
 	content: string
 }
 
+type sendTo = {
+	email: string,
+	name?: string
+}
+
+//mailchannels api docs: https://api.mailchannels.net/tx/v1/documentation
+
 export default {
 	async fetch(
 		request: Request,
 		env: Env,
-		ctx: ExecutionContext
+		_ctx: ExecutionContext
 	): Promise<Response> {
-		if (request.method == "POST") {
+		if (request.method != "POST") return new Response("Method not allowed", { status: 405 });
+		if (request.headers.get("Authorization") != env.AUTH_TOKEN) return new Response("Unauthorized", { status: 401 });
+		try {
 			const req = await request.json() as email
-			try {
-				let send_request = new Request("https://api.mailchannels.net/tx/v1/send", {
-					"method": "POST",
-					"headers": {
-						"content-type": "application/json",
+
+			if (!req.to) return new Response("No recipient", { status: 400 });
+			if (!req.subject) return new Response("No subject", { status: 400 });
+			if (!req.content) return new Response("No content", { status: 400 });
+
+			if (req.from && !req.from.includes(`@${env.DKIM_DOMAIN}`))
+				return new Response("You must use a domain that matches your DKIM domain", { status: 400 });
+
+			const send_request = new Request("https://api.mailchannels.net/tx/v1/send", {
+				"method": "POST",
+				"headers": {
+					"content-type": "application/json",
+				},
+				"body": JSON.stringify({
+					"personalizations": [{
+						"to": req.to,
+						"cc": req.cc,
+						"bcc": req.bcc,
+						"reply_to": req.replyTo,
+						"dkim_domain": env.DKIM_DOMAIN,
+						"dkim_selector": env.DKIM_SELECTOR,
+						"dkim_private_key": env.DKIM_PRIVATE_KEY,
+					}],
+					"from": {
+						"email": `${req.from || env.EMAIL_DEFAULT_FROM}@${env.DKIM_DOMAIN}`,
+						"name": `${req.fromName || env.EMAIL_DEFAULT_FROMNAME}`,
 					},
-					"body": JSON.stringify({
-						"personalizations": [{
-							"to": [{
-								"email": req.to,
-							}],
-							"dkim_domain": env.DKIM_DOMAIN,
-							"dkim_selector": env.DKIM_SELECTOR,
-							"dkim_private_key": env.DKIM_PRIVATE_KEY
-						}],
-						"from": {
-							"email": `${req.from || env.EMAIL_DEFAULT_FROM}@${env.DKIM_DOMAIN}`,
-							"name": `${req.fromName || env.EMAIL_DEFAULT_FROMNAME}`,
-						},
-						"subject": req.subject,
-						"content": [{
-							"type": `${req.html ? "text/html" : "text/plain"}`,
-							"value": req.content,
-						}],
-					}),
-				});
-				const res = await fetch(send_request)
-				const respText = await res.text();
-				return new Response("Email sent " + respText);
-			} catch (err) {
-				return new Response("Erro", { status: 500 });
-			}
-		} else {
-			return new Response("Method not allowed", { status: 405 });
+					"subject": req.subject,
+					"content": [{
+						"type": `${req.html ? "text/html" : "text/plain"}`,
+						"value": req.content,
+					}],
+				}),
+			});
+			const res = await fetch(send_request)
+			if (!res.ok) return new Response(`Error api status:${res.status}`, { status: 500 });
+			return new Response("Email sent", { status: 200 });
+		} catch (err) {
+			return new Response("Error", { status: 500 });
 		}
 	}
 }
